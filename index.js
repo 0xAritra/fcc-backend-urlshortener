33d29b70bf518631903d503ed9b9f484b12fd988
Aritra <ari@aritra.xyz>
2022-10-25 12:35:30 +0530

    project done

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const bodyParser = require("body-parser")
+const dns = require("dns")
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(cors());
+app.use(cors())
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`))
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html")
+})
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" })
+})
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.use(bodyParser.urlencoded({ extended: false }))
+
+let count = 0
+const idToLink = {}
+const LinkToId = {}
+
+app.post("/api/shorturl/", function (req, res) {
+  let url = req.body.url
+  const regex = /^https?:\/\//
+  if (!regex.test(url)) {
+    res.json({ error: "Invalid URL" })
+    return
+  }
+  url = new URL(url)
+  dns.lookup(url.hostname, (err, addresses) => {
+    if (err) res.json({ error: "Invalid Hostname" })
+    else {
+      if (LinkToId[url])
+        res.json({
+          original_url: url,
+          short_url: LinkToId[url],
+        })
+      // for existing urls
+      else {
+        count++
+        idToLink[count] = url
+        LinkToId[url] = count
+        res.json({ original_url: url, short_url: count })
+      }
+    }
+  })
+})
+
+app.get("/api/shorturl/:id", function (req, res) {
+  const url = idToLink[req.params.id]
+  if (url) res.status(301).redirect(url)
+  else res.json({ error: "No short URL found for the given input" })
+})
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
+})
